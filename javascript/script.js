4c790742953580f65147aae296911f0618e3b8bf
thinksolution <thinksolution2014@gmail.com>
2022-10-10 23:06:34 +0530

    hosting

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+
+
+// Navbar togglemenu icons
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function togglemenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "130px";
+    }
+    else {
+        menuList.style.maxHeight = "0px";
+    }
+}
+
+// function changeImg(id) {
+//     alert(id)
+// }
